273c053b10142529c48b45c745e247a02d1188b1
sadebare <adebaresulaimon04@gmail.com>
2022-12-22 00:48:05 +0100

    task 3: promise.then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({
+        status: 200,
+        body: 'success',
+      });
+    } else {
+      reject(new Error());
+    }
+  }).then(() => {
+    console.log('Got a response from the API');
+  });
+}
